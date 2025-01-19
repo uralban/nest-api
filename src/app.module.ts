117d6e1a680ae7e18a8d02939027902d3cbaa6e5
Alex Simzikov <uralban@gmail.com>
2025-01-19 18:00:38 +0200

    feat: set TypeOrm connection to db; configure docker's db container; update README.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import {INestApplication, Module} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+require('dotenv').config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

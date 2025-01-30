ddf691280ea1043fa2b47dcc5aef13278d5aea20
Alex Simzikov <uralban@gmail.com>
2025-01-30 18:36:55 +0200

    fix: turn on SSL in typeorm settings

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HealthCheckData } from './global/interfaces/health-check-data';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('API')
@@ -10,11 +9,7 @@ export class AppController {
 
   @Get()
   @ApiOperation({ summary: 'Get a health check data.' })
-  public async getHealthCheck(): Promise<HealthCheckData> {
-    try {
-      return await this.appService.getHealthCheck();
-    } catch (e) {
-      console.error(e);
-    }
+  public async getHealthCheck(): Promise<string> {
+    return await this.appService.getHealthCheck();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,12 @@
-import { HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { HealthCheckData } from './global/interfaces/health-check-data';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
 
-  public async getHealthCheck(): Promise<HealthCheckData> {
+  public async getHealthCheck(): Promise<string> {
     return new Promise(resolve => {
-      resolve({
-        status_code: HttpStatus.OK,
-        detail: 'ok',
-        result: 'working',
-      });
+      resolve('ok');
       this.logger.log('Getting health check is OK');
     });
   }
